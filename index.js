1c4af2a4e62cc7794908cf355ffcd8ad41015fee
Jessica Schlauderaff <jessicaschlauderaff@gmail.com>
2017-09-10 18:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function whisper(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if(string === lowercase) {
+    return "I can't hear you!"
+  }
+  if(string === uppercase) {
+    return "YES INDEED!"
+  }
+  if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
